f8751b507722d3ca4fe7e36eba42ad472708f804
Charlie <new791104@gmail.com>
2019-01-07 12:37:31 +0800

    main.c update

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <io.h>
 #include <stdlib.h>
 #include <string.h>
 void initOutput(FILE*);
@@ -11,32 +10,17 @@ extern FILE *yyout;
 int main(int argc, char* argv[]) {
     yyout = fopen("mips.txt", "w+");
     _Bool debug_mode = 0;
-    // initOutput(yyout);
+    int test = 0;
 
-    if (strcmp(argv[1], "-p") == 0) {
-        debug_mode = 1;    
+    if (argc == 2 && strcmp(argv[1], "-p") == 0) {
+        yyparse();   
     }
-
-    if (argc == 1 && debug_mode) {
-        printf("#### You can use \" parse -p < file\" to parse the file. ####\n");
-        yyparse();
-    }
-    else if (argc == 2 && debug_mode) {
-        yyin = fopen(argv[1], "r");
-        yyparse();
-    }
-    else if (argc == 3 && debug_mode) {
-        yyout = fopen(argv[2],"w+");
-        yyin = fopen(argv[1], "r");
-        yyparse();
+    else {
+        printf("#### You can use \"parse -p < filename\" to parse the file. ####\n");
+        printf("#### Output will be \"mips.txt\" ####\n");
     }
     fclose(yyin);
     fclose(yyout);
 
 	return 0;
 }
-
-// void initOutput(FILE *file) {
-//     char initString[] = "    .data\n    .text";
-//     fwrite(initString, 1, strlen(initString), file);
-// }
